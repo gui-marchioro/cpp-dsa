26082e55511db381f29b0a2826c8f7e735637f7a
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2026-06-16 22:51:05 -0300

    Re-solved 133 Clone graph now using DFS instead of BFS

---
+++ src/blind_75/133 _clone-graph/solution/main.cpp
@@ -0,0 +1,70 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+/*  -- Graph DFS traversal
+        - given a root node
+        - have a hashmap containing val as a key and Node instance
+        - traverse nodes using DFS
+            - returning cloned node if exists
+            - or creating new clone if not
+        - avoid to repeat looking for nodes already in the hashmap
+    -- Time complexity: O(n)
+        - all TreeNodes are visited at least once
+    -- Space complexity: average O(log n), worst O(n)
+        - In a completely unbalanced tree all nodes would be kept in memory
+
+    -- Leet Code submission results:
+        Runtime: 3 ms
+        Beats: 68.30%
+
+        Memory: 12.22 MB
+        Beats: 26.88%
+*/
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return nullptr;
+        }
+
+        unordered_map<int, Node*> clones;
+        return dfs(node, clones);
+    }
+private:
+    Node* dfs(Node* node, unordered_map<int, Node*>& clones) {
+        if (clones.count(node->val)) {
+            return clones[node->val];
+        }
+
+        Node* clone = new Node(node->val);
+        clones[node->val] = clone;
+        for (auto& neigh : node->neighbors) {
+            clone->neighbors.push_back(dfs(neigh, clones));
+        }
+        return clone;
+    }
+};
